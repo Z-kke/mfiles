6acca7d070346b825f5668c16b8e8323796df568
Saku Arho <sakurho@gmail.com>
2024-08-28 19:28:13 +0300

    Add ProjectContext with type.

---
+++ problem-3-project_manager/src/context/ProjectContext.tsx
@@ -0,0 +1,19 @@
+import React, { createContext, useContext, useState } from "react";
+import { Project, ProjectState } from "../types/Project";
+
+interface ProjectContextType {
+  projects: Project[];
+  selectedProjects: number[];
+  toggleProjectSelection: (id: number) => void;
+  clearSelection: () => void;
+  updateProjectState: (newState: ProjectState) => void;
+}
+
+const initialData: Project[] = [
+  { id: 1, name: "Project A", state: "Not Started" },
+  { id: 2, name: "Project B", state: "Not Started" },
+  { id: 3, name: "Project C", state: "Launched" },
+  { id: 4, name: "Project D", state: "Finished" },
+];
+
+const ProjectContext = createContext<ProjectContextType | undefined>(undefined);
