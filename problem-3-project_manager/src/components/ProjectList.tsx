12f04a38174541660e7adc8219a4c16abc5d3571
Saku Arho <sakurho@gmail.com>
2024-08-28 19:57:22 +0300

    Add styling to the ListItem based on their states.

--- problem-3-project_manager/src/components/ProjectList.tsx
+++ problem-3-project_manager/src/components/ProjectList.tsx
@@ -3,12 +3,30 @@ import { List, ListItem, ListItemText } from "@mui/material";
 import { useProjects } from "../context/ProjectContext";
 
 const ProjectList: React.FC = () => {
-  const { projects } = useProjects();
+  const { projects, selectedProjects, toggleProjectSelection } = useProjects();
 
   return (
     <List>
       {projects.map((project) => (
-        <ListItem key={project.id}>
+        <ListItem
+          key={project.id}
+          onClick={() => toggleProjectSelection(project.id)}
+          style={{
+            margin: 8,
+            cursor: project.state === "Finished" ? "not-allowed" : "pointer",
+            backgroundColor: selectedProjects.includes(project.id)
+              ? "lightblue"
+              : "white",
+            color: selectedProjects.includes(project.id) ? "white" : "black",
+            borderRight: `35px solid ${
+              project.state === "Not Started"
+                ? "yellow"
+                : project.state === "Launched"
+                  ? "green"
+                  : "gray"
+            }`,
+          }}
+        >
           <ListItemText primary={project.name} secondary={project.state} />
         </ListItem>
       ))}
