546658c60bb604009fb2f8e2249017646e3e6721
Saku Arho <sakurho@gmail.com>
2024-08-28 19:52:24 +0300

    Add the components wrapped inside of the context provider to App.tsx.

--- problem-3-project_manager/src/App.tsx
+++ problem-3-project_manager/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container } from "@mui/material";
+import ProjectList from "./components/ProjectList";
+import ControlPanel from "./components/ControlPanel";
+import { ProjectProvider } from "./context/ProjectContext";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProjectProvider>
+      <Container>
+        <h1>Project Manager</h1>
+        <ProjectList />
+        <ControlPanel />
+      </Container>
+    </ProjectProvider>
   );
-}
+};
 
 export default App;
