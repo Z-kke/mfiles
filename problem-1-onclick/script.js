19291dc8b31499b1d129edd3fc3061beae8b17d4
Saku Arho <sakurho@gmail.com>
2024-08-28 17:36:20 +0300

    Add the solution (Modify the function)
    I used this solution as if this would be a bigger project with multiple
    other developers on it and the function was used with strings elsewhere
    then changing it to work without strings would cause a change in how it
    was previously implemented.
    
    Eval and Function constructors could have also been used, but these are
    not recommended by JS docs as they cause security concerns, mostly XSS.

--- problem-1-onclick/script.js
+++ problem-1-onclick/script.js
@@ -3,7 +3,33 @@ function suggestionElementPreparation(
   suggestionClickEvent,
 ) {
   const suggestionElem = document.getElementById("mf-suggestion-event");
-  suggestionElem.onclick = suggestionClickEvent;
+
+  // Define map of possible function calls
+  const functionMap = {
+    "webURLPreparation( true, false )": () => webURLPreparation(true, false),
+    // Add other function calls used by this
+  };
+
+  if (typeof suggestionClickEvent === "string") {
+    if (functionMap[suggestionClickEvent]) {
+      suggestionElem.onclick = functionMap[suggestionClickEvent];
+    } else {
+      console.error(
+        "Error: String of the function is not defined in the function map.",
+      );
+    }
+  } else if (typeof suggestionClickEvent === "function") {
+    suggestionElem.onclick = suggestionClickEvent;
+  } else {
+    console.error(
+      "Error: suggestionClickEvent should be either string or a function.",
+    );
+  }
+}
+
+// Example function to be called on click
+function webURLPreparation(arg1, arg2) {
+  console.log("webURLPreparation called with arguments:", arg1, arg2);
 }
 
 // Example how this function is called:
