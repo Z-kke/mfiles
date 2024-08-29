f1576de780e388ff48a3cf3fd4a99d9748075d56
Saku Arho <sakurho@gmail.com>
2024-08-29 10:04:59 +0300

    Add logic for giving a light blue color on hover if the task is selectable.

--- problem-3-project_manager/src/components/ProjectList.tsx
+++ problem-3-project_manager/src/components/ProjectList.tsx
@@ -5,58 +5,74 @@ import { useProjects } from "../context/ProjectContext";
 const ProjectList: React.FC = () => {
   const { projects, selectedProjects, toggleProjectSelection } = useProjects();
 
+  // Determine the state of the first selected project, if any
+  const selectedState = selectedProjects.length
+    ? projects.find((project) => selectedProjects.includes(project.id))?.state
+    : null;
+
   return (
     <List>
-      {projects.map((project) => (
-        <Paper
-          key={project.id}
-          elevation={1}
-          sx={{
-            width: "100%",
-            marginBottom: 2,
-          }}
-        >
-          <ListItem
-            onClick={() => toggleProjectSelection(project.id)}
-            style={{
-              cursor: project.state === "Finished" ? "not-allowed" : "pointer",
-              backgroundColor: selectedProjects.includes(project.id)
-                ? "darkblue"
-                : "white",
-              color: selectedProjects.includes(project.id) ? "white" : "black",
-              borderRight: `25px solid ${
-                project.state === "Not Started"
-                  ? "lightyellow"
-                  : project.state === "Launched"
-                    ? "lightgreen"
-                    : "lightgray"
-              }`,
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              alignItems: "center",
-              height: "75px",
-              padding: "0",
+      {projects.map((project) => {
+        const isSelectable =
+          project.state !== "Finished" &&
+          (!selectedState || selectedState === project.state);
+
+        return (
+          <Paper
+            key={project.id}
+            elevation={1}
+            sx={{
+              width: "100%",
+              marginBottom: 2,
             }}
           >
-            <Box
+            <ListItem
+              onClick={() => isSelectable && toggleProjectSelection(project.id)}
               sx={{
-                flexGrow: 1,
+                cursor: isSelectable ? "pointer" : "not-allowed",
+                backgroundColor: selectedProjects.includes(project.id)
+                  ? "darkblue"
+                  : "white",
+                color: selectedProjects.includes(project.id)
+                  ? "white"
+                  : "black",
+                borderRight: `25px solid ${
+                  project.state === "Not Started"
+                    ? "lightyellow"
+                    : project.state === "Launched"
+                      ? "lightgreen"
+                      : "lightgray"
+                }`,
                 display: "flex",
                 flexDirection: "column",
                 justifyContent: "space-between",
-                width: "100%",
-                textAlign: "center",
+                alignItems: "center",
+                height: "75px",
+                padding: "0",
+                "&:hover": {
+                  backgroundColor: isSelectable ? "lightblue" : "white",
+                },
               }}
             >
-              <Typography variant="body1" sx={{ fontWeight: "bold" }}>
-                {project.name}
-              </Typography>
-              <Typography variant="body2">{project.state}</Typography>
-            </Box>
-          </ListItem>
-        </Paper>
-      ))}
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                  width: "100%",
+                  textAlign: "center",
+                }}
+              >
+                <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+                  {project.name}
+                </Typography>
+                <Typography variant="body2">{project.state}</Typography>
+              </Box>
+            </ListItem>
+          </Paper>
+        );
+      })}
     </List>
   );
 };
